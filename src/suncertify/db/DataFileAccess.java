2be9d62161a74b517cb9d5dee4ce42cae2ee56c3
nrkkalyan <nrkkalyan@gmail.com>
2011-08-26 00:36:08 +0200

    d

--- src/suncertify/db/DataFileAccess.java
+++ src/suncertify/db/DataFileAccess.java
@@ -2,7 +2,10 @@ package suncertify.db;
 
 import java.io.File;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
@@ -11,6 +14,7 @@ import java.util.concurrent.locks.ReentrantLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import suncertify.common.ApplicationException;
+import suncertify.common.ErrorCodes;
 
 public final class DataFileAccess implements Serializable {
 	
@@ -40,7 +44,7 @@ public final class DataFileAccess implements Serializable {
 	// Position where a new record should be created
 	private final int					mNextAvailablePosition	= 0;
 	
-	public DataFileAccess(String path) {
+	private DataFileAccess(String path) throws ApplicationException {
 		if (path == null || !(new File(path).exists())) {
 			throw new IllegalArgumentException("Invalid database file path.");
 		}
@@ -56,5 +60,97 @@ public final class DataFileAccess implements Serializable {
 		} finally {
 			mReadWriteLock.writeLock().unlock();
 		}
+	}
+	
+	/**
+	 * Returns the {@link DataFileAccess} instance. It is implemented as a
+	 * Singleton Pattern
+	 * 
+	 * @param dbPath
+	 * @return {@link DataFileAccess}
+	 * @throws ApplicationException
+	 * */
+	public static synchronized DataFileAccess getInstance(String dbPath) throws ApplicationException {
+		if (mDataFileAccess == null) {
+			mDataFileAccess = new DataFileAccess(dbPath);
+		}
+		return mDataFileAccess;
+	}
+	
+	public int create(String[] data) {
+		
+		try {
+			mReadWriteLock.writeLock().lock();
+			int positionToInsert = mNextAvailablePosition;
+			Room room = RoomConverter.roomFromStringArray(data, positionToInsert);
+			
+			mNextAvailablePosition += DBConstants.RECORD_LENGTH;
+			mRecordMap.put(positionToInsert, room);
+			
+			return positionToInsert;
+		} finally {
+			mReadWriteLock.writeLock().unlock();
+		}
+	}
+	
+	public void delete(int recordId) throws RecordNotFoundException {
+		try {
+			mReadWriteLock.writeLock().lock();
+			Room room = mRecordMap.get(recordId);
+			if (room != null && room.isValid()) {
+				room.setIsValid(false);
+			} else {
+				throw new RecordNotFoundException(ErrorCodes.RECORD_NOT_FOUND.name());
+			}
+			
+		} finally {
+			mReadWriteLock.writeLock().unlock();
+		}
+	}
+	
+	public String[] read(int recordId) throws RecordNotFoundException {
+		try {
+			mReadWriteLock.writeLock().lock();
+			
+			Room room = mRecordMap.get(recordId);
+			if (room != null && room.isValid()) {
+				return RoomConverter.stringArrayFromRoom(room);
+			} else {
+				throw new RecordNotFoundException(ErrorCodes.RECORD_NOT_FOUND.name());
+			}
+		} finally {
+			mReadWriteLock.writeLock().unlock();
+		}
+	}
+	
+	public void update(int recordId, String[] data) {
+		Room room = RoomConverter.roomFromStringArray(data, recordId);
+		update(recordId, room);
+	}
+	
+	private void update(int recordId, Room room) {
+		try {
+			mReadWriteLock.writeLock().lock();
+			mRecordMap.put(recordId, room);
+		} finally {
+			mReadWriteLock.writeLock().unlock();
+		}
+	}
+	
+	public Integer[] find(String[] criteria) {
+		Room room = RoomConverter.roomFromStringArray(criteria, null);
+		List<Integer> recordList = new ArrayList<Integer>();
+		try {
+			mReadWriteLock.readLock().lock();
+			Collection<Room> rooms = mRecordMap.values();
+			for (Room r : rooms) {
+				if (r.match(room)) {
+					recordList.add(r.getRecordId());
+				}
+			}
+		} finally {
+			mReadWriteLock.readLock().unlock();
+		}
+		return (Integer[]) recordList.toArray();
 	}
 }

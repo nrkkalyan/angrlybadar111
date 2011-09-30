7e03c326014855a8d76a49b5004d6b9ae41bf5ea
nrkkalyan <nrkkalyan@gmail.com>
2011-09-30 08:09:01 +0200

    changes in properties

---
+++ src/suncertify/gui/ActionCommand.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package suncertify.gui;
+
+/**
+ * @author Koosie
+ * 
+ */
+public enum ActionCommand {
+	
+	EXIT, CONNECT_LOCAL, CONNECT_REMOTE, DISCONNECT, VIEWALL, SEARCH, SEARCH_WITH_PARAM, BOOK
+	
+}
